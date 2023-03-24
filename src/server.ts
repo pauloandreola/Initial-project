db8a1a574209b5b0edd9a22b132cc6d16360759d
Paulo Andreola <pauloandreola@gmail.com>
2023-03-24 07:53:35 -0300

    feat: first endpoint test get method with hello world

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,8 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  return res.status(200).json('Hello World!');
+})
+
 app.listen(port, () => (console.log(`Server running at port => ${port}`)));
