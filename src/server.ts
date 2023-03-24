0de235239130f3bd9e39e9522e6c3a12214ac4c4
Paulo Andreola <pauloandreola@gmail.com>
2023-03-24 20:50:57 -0300

    feat: update format get endpoint server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.status(200).json('Hello World!'));
+app.get('/', (req, res) => {
+  res.status(200).json('Hello World!');
+});
 
 app.listen(port, () => (console.log(`Server running at port => ${port}`)));
